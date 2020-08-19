b466483d24824233af9c170bf5565373abb4970e
bctm <alberto_badia@enlacepatagonia.com>
2020-08-19 12:45:52 -0300

    IMPROVE: Refactoring - per feature package

---
+++ api/interface/__init__.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter
+
+from .interface import interface_router
+from .interface_list import interface_list_router
+from .interface_list_member import interface_list_member_router
+from .interface_ethernet import interface_ethernet_router
+from .interface_eoip import interface_eoip_router
+from .interface_ipip import interface_ipip_router
+
+interface_router = APIRouter()
+
+interface_eoip_router.include_router(
+    interface_router,
+    prefix=""
+)
+
+interface_router.include_router(
+    interface_list_router,
+    prefix="/list"
+)
+
+interface_router.include_router(
+    interface_list_member_router,
+    prefix="/list/member"
+)
+
+interface_router.include_router(
+    interface_ethernet_router,
+    prefix="/ethernet"
+)
+
+interface_router.include_router(
+    interface_eoip_router,
+    prefix="/eoip"
+)
+
+interface_router.include_router(
+    interface_ipip_router,
+    prefix="/ipip"
+)
--- main.py
+++ main.py
@@ -1,37 +1,8 @@
 from app import APP
+
 from api.interface import interface_router
-from api.interface_list import interface_list_router
-from api.interface_list_member import interface_list_member_router
-from api.interface_ethernet import interface_ethernet_router
-from api.interface_eoip import interface_eoip_router
-from api.interface_ipip import interface_ipip_router
 
 APP.include_router(
     interface_router,
     prefix="/interface"
-)
-
-APP.include_router(
-    interface_list_router,
-    prefix="/interface/list"
-)
-
-APP.include_router(
-    interface_list_member_router,
-    prefix="/interface/list/member"
-)
-
-APP.include_router(
-    interface_ethernet_router,
-    prefix="/interface/ethernet"
-)
-
-APP.include_router(
-    interface_eoip_router,
-    prefix="/interface/eoip"
-)
-
-APP.include_router(
-    interface_ipip_router,
-    prefix="/interface/ipip"
 )
